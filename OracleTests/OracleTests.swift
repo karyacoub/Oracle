ed5bf724aff36fdf8693b44638e3ab1515d5f9f2
Kareem Yacoub <karyacoub@gmail.com>
2025-06-15 15:36:06 -0500

    Initial Commit

---
+++ OracleTests/OracleTests.swift
@@ -0,0 +1,17 @@
+//
+//  OracleTests.swift
+//  OracleTests
+//
+//  Created by Kareem Yacoub on 6/15/25.
+//
+
+import Testing
+@testable import Oracle
+
+struct OracleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
