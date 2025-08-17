7b968bc0e8e5387eb908805add9864bac463fd0d
Kareem Yacoub <karyacoub@gmail.com>
2025-08-17 00:04:18 -0500

    Library page skeleton

--- Oracle/Components/AlbumTile.swift
+++ Oracle/Components/AlbumTile.swift
@@ -2,15 +2,21 @@ import SwiftUI
 
 struct AlbumTile: View {
     var body: some View {
-        
-        Image(systemName: "music.note")
-            .resizable()
-            .scaledToFit()
-            .padding(50)
-            .frame(width: 170, height: 170)
-            .foregroundColor(Colors.DARK_GRAY)
-            .background(Colors.GRAY)
-            .cornerRadius(10)
+        VStack {
+            Image(systemName: "music.note")
+                .resizable()
+                .scaledToFit()
+                .padding(50)
+                .frame(width: 170, height: 170)
+                .foregroundColor(Colors.DARK_GRAY)
+                .background(Colors.GRAY)
+                .cornerRadius(10)
+            Text("Album Title")
+                .bold()
+            Text("Artist")
+                .foregroundColor(Colors.DARK_GRAY)
+            
+        }
     }
 }
 
--- Oracle/Views/LibraryView.swift
+++ Oracle/Views/LibraryView.swift
@@ -8,10 +8,58 @@ struct LibraryView: View {
                 .padding(.bottom)
                 .bold()
             
+            let columns = [
+                GridItem(), // left column
+                GridItem()  // right column
+            ]
+            LazyVGrid(columns: columns) {
+                HStack {
+                    Text("Songs")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Image(systemName: "music.note")
+                }
+                .padding(.all)
+                .background(Colors.GRAY)
+                
+                HStack {
+                    Text("Albums")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Image(systemName: "rectangle.stack.fill")
+                }
+                .padding(.all)
+                .background(Colors.GRAY)
+                
+                HStack {
+                    Text("Playlists")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Image(systemName: "music.note.list")
+                }
+                .padding(.all)
+                .background(Colors.GRAY)
+                
+                HStack {
+                    Text("Favorites")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    Image(systemName: "heart.fill")
+                }
+                .padding(.all)
+                .background(Colors.GRAY)
+            }
+            .padding(.trailing)
+            .padding(.bottom)
+            .cornerRadius(10)
+            
             Text("Recently Played")
+                .font(.title)
+
+            AlbumTileList()
+                .padding(.bottom)
+            
+            Text("Recently Added")
                 .font(.title)
                 
             AlbumTileList()
+                .padding(.bottom)
         }
         .frame(
             maxWidth: .infinity,
